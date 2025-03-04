a3292b3d453054b8de6d83d49cd7cad2530f7d14
Divyanshi <mailto:your-emai>
2025-03-04 10:28:44 +0530

    Margin Removed

--- src/components/SvgIcon/Icons/EmptyCanvas.tsx
+++ src/components/SvgIcon/Icons/EmptyCanvas.tsx
@@ -1,7 +1,12 @@
 export default () => {
   return (
-
-    <svg width="253" height="136" viewBox="0 0 253 136" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="253"
+      height="136"
+      viewBox="0 0 253 136"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <rect x="21" width="214" height="136" rx="10" fill="#D9D9D9" />
       <g filter="url(#filter0_d_247_640)">
         <rect x="47" y="72" width="199" height="40" rx="10" fill="white" />
@@ -14,28 +19,77 @@ export default () => {
         <rect x="15" y="31" width="15" height="15" rx="2" fill="#D9D9D9" />
       </g>
       <defs>
-        <filter id="filter0_d_247_640" x="40" y="68" width="213" height="54" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <filter
+          id="filter0_d_247_640"
+          x="40"
+          y="68"
+          width="213"
+          height="54"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
           <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
           <feOffset dy="3" />
           <feGaussianBlur stdDeviation="3.5" />
           <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
-          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_247_640" />
-          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_247_640" result="shape" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_247_640"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_247_640"
+            result="shape"
+          />
         </filter>
-        <filter id="filter1_d_247_640" x="0" y="14" width="213" height="54" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <filter
+          id="filter1_d_247_640"
+          x="0"
+          y="14"
+          width="213"
+          height="54"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
           <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
           <feOffset dy="3" />
           <feGaussianBlur stdDeviation="3.5" />
           <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
-          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_247_640" />
-          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_247_640" result="shape" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_247_640"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_247_640"
+            result="shape"
+          />
         </filter>
       </defs>
     </svg>
-
   );
 };
--- src/containers/CanvasContainer/EmptyBlock.tsx
+++ src/containers/CanvasContainer/EmptyBlock.tsx
@@ -13,7 +13,6 @@ const EmptyBlockContainer = styled.div`
   font-size: 2rem;
   border: 1px dashed #DDDDDD;
   // border-radius: 10px;
-  margin-top: 0.5rem;
 `;
 
 const MessageText = styled.div`
