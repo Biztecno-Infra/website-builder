4996247849eeaa4dbabb417d93552275783e96e2
Ayushgautam2401 <ayush.gautam@biztecno.net>
2025-03-07 12:15:45 +0530

    clipPathWarning

--- src/components/SvgIcon/Icons/FileTypeJson.tsx
+++ src/components/SvgIcon/Icons/FileTypeJson.tsx
@@ -7,7 +7,7 @@ export default () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_97_389)">
+      <g clipPath="url(#clip0_97_389)">
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
