82e66a737a590302aa527a583f52103dc0d638b6
Divyanshi <mailto:your-emai>
2025-03-12 17:27:57 +0530

    Html function Changes

--- src/containers/HeaderActions/HeaderActions.tsx
+++ src/containers/HeaderActions/HeaderActions.tsx
@@ -58,7 +58,6 @@ function HeaderActions({ onExport }: Props) {
 
   const handleExport = (format: 'JSON' | 'HTML') => {
     const convertedData = format === 'JSON' ? blocksToJson() : convertJsonToHtml(blocksToJson());
-    console.log("handleExport" , convertedData)
     if(typeof onExport === "function"){
       onExport(format, convertedData);
     }
