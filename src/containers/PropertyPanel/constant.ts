9c5edeab636720126f2298ecab0cca55bd1c470d
Divyanshi90 <divyanshi@biztecno.net>
2025-06-25 18:50:22 +0530

    feat: Added Options and updated values for Font family

--- src/containers/PropertyPanel/constant.ts
+++ src/containers/PropertyPanel/constant.ts
@@ -1,17 +1,41 @@
+// export const fontOptions = [
+//   { key: "modern_sans", text: "Modern Sans", value: "Modern Sans" },
+//   { key: "arial", text: "Arial", value: "Arial" },
+//   { key: "times", text: "Times New Roman", value: "Times New Roman" },
+//   { key: "courier", text: "Courier New", value: "Courier New" },
+//   { key: "georgia", text: "Georgia", value: "Georgia" },
+//   { key: "verdana", text: "Verdana", value: "Verdana" },
+//   { key: "tahoma", text: "Tahoma", value: "Tahoma" },
+//   { key: "comic", text: "Comic Sans MS", value: "Comic Sans MS" },
+//   { key: "helvetica", text: "Helvetica", value: "Helvetica" },
+//   { key: "lucida", text: "Lucida Sans", value: "Lucida Sans" },
+//   { key: "palatino", text: "Palatino Linotype", value: "Palatino Linotype" },
+//   { key: "trebuchet", text: "Trebuchet MS", value: "Trebuchet MS" },
+// ];
+
 export const fontOptions = [
   { key: "modern_sans", text: "Modern Sans", value: "Modern Sans" },
-  { key: "arial", text: "Arial", value: "Arial" },
-  { key: "times", text: "Times New Roman", value: "Times New Roman" },
-  { key: "courier", text: "Courier New", value: "Courier New" },
-  { key: "georgia", text: "Georgia", value: "Georgia" },
-  { key: "verdana", text: "Verdana", value: "Verdana" },
-  { key: "tahoma", text: "Tahoma", value: "Tahoma" },
-  { key: "comic", text: "Comic Sans MS", value: "Comic Sans MS" },
-  { key: "helvetica", text: "Helvetica", value: "Helvetica" },
-  { key: "lucida", text: "Lucida Sans", value: "Lucida Sans" },
-  { key: "palatino", text: "Palatino Linotype", value: "Palatino Linotype" },
-  { key: "trebuchet", text: "Trebuchet MS", value: "Trebuchet MS" },
+  { key: "arial", text: "Arial", value: "Arial, sans-serif" },
+  { key: "helvetica", text: "Helvetica", value: "Helvetica, Arial, sans-serif" },
+  { key: "sans_serif", text: "Default Sans-serif", value: "sans-serif" },
+  { key: "times", text: "Times New Roman", value: "'Times New Roman', serif" },
+  { key: "courier", text: "Courier New", value: "'Courier New', monospace" },
+  { key: "georgia", text: "Georgia", value: "Georgia, serif" },
+  { key: "verdana", text: "Verdana", value: "Verdana, sans-serif" },
+  { key: "tahoma", text: "Tahoma", value: "Tahoma, sans-serif" },
+  { key: "comic", text: "Comic Sans MS", value: "'Comic Sans MS', cursive, sans-serif" },
+  { key: "lucida", text: "Lucida Sans", value: "'Lucida Sans', sans-serif" },
+  { key: "palatino", text: "Palatino Linotype", value: "'Palatino Linotype', 'Book Antiqua', Palatino, serif" },
+  { key: "trebuchet", text: "Trebuchet MS", value: "'Trebuchet MS', sans-serif" },
+
+  // Additional modern web fonts
+  { key: "roboto", text: "Roboto", value: "Roboto, sans-serif" },
+  { key: "opensans", text: "Open Sans", value: "'Open Sans', sans-serif" },
+  { key: "segoe_ui", text: "Segoe UI", value: "'Segoe UI', sans-serif" },
+  { key: "lato", text: "Lato", value: "Lato, sans-serif" },
+  { key: "merriweather", text: "Merriweather", value: "Merriweather, serif" }
 ];
+
 
 export const fontWeightOptions = [
   { key: "normal", text: "Normal (400)", value: "400" },
